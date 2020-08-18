cbf57ead0da3a3112cff00d7d1a0bfa250debb54
anhphamdt98 <pham.hai.anh@sun-asterisk.com>
2020-08-18 14:56:03 +0700

    init models and create relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,34 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role() 
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function posts() 
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likes() 
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments() 
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'user_followers', 'follower_id', 'user_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_followers', 'user_id', 'follower_id');
+    }
 }
